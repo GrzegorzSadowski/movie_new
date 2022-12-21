2b74d48197cd09016fd37553bd0c83f8013759fd
GrzegorzSadowski <gsadowski@op.pl>
2022-12-21 13:15:20 +0100

    Initial commit

---
+++ src/main/java/com/exadel/movie_new/MovieNewApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.movie_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNewApplication.class, args);
+    }
+
+}
