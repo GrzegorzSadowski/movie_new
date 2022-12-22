02ef143dd65d94263104e3a174c7368f4e57d8f4
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2022-12-22 10:21:04 +0100

    Update MovieController.java

--- src/main/java/com/exadel/movie_new/controller/MovieController.java
+++ src/main/java/com/exadel/movie_new/controller/MovieController.java
@@ -2,33 +2,44 @@ package com.exadel.movie_new.controller;
 
 import com.exadel.movie_new.model.Movie;
 import com.exadel.movie_new.service.MovieService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class MovieController {
-    @Autowired
-    MovieService movieService;
 
-    @PostMapping("/movie")
+    private MovieService movieService;
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+
+    @PostMapping("/addmovie")
     public Movie addMovie(@RequestBody Movie movie) {
 
         return movieService.addMovie(movie);
     }
 
-    @GetMapping("/movie/{movieId}")
-    public Movie getMovieDetails(@PathVariable String movieId) {
+    @GetMapping("/getmovie/{movieId}")
+    public Movie getMovie(@PathVariable String movieId) {
 
-        return movieService.getMovieDetails(movieId);
+        return movieService.getMovie(movieId);
     }
 
-    @PutMapping("/movie/{movieId}")
+    @GetMapping("/getallmovies")
+    public List<Movie> getAllMovies() {
+
+        return movieService.getAllMovies();
+    }
+
+    @PutMapping("/updatemovie/{movieId}")
     public Movie updateMovie(@RequestBody Movie movie) {
         return movieService.updateMovie(movie);
     }
 
-    @DeleteMapping("movie/{movieId}")
+    @DeleteMapping("deletemovie/{movieId}")
     public void deleteMovie(@PathVariable String movieId) {
         movieService.deleteMovie(movieId);
     }
