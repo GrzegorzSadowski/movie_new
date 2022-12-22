c8a96de0ea2bde97204775e6384d25459c7df644
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2022-12-22 10:23:16 +0100

    Update MovieServiceImpl.java

--- src/main/java/com/exadel/movie_new/service/MovieServiceImpl.java
+++ src/main/java/com/exadel/movie_new/service/MovieServiceImpl.java
@@ -4,7 +4,6 @@ import com.exadel.movie_new.model.Movie;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Service;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,24 +18,32 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie addMovie(Movie movie) {
         List<Movie> movieData = readJsonData();
-        Movie moviePresent = null;
+       // Movie moviePresent = null;
 
+        boolean isPresent = false;
         if (movieData != null) {
-            moviePresent = movieData.parallelStream().filter(mov -> mov.getId().equals(movie.getId())).findAny().orElse(null);
+            //moviePresent = movieData.parallelStream().filter(mov -> mov.getId().equals(movie.getId())).findAny().orElse(null);
+            isPresent = movieData.parallelStream().anyMatch(mov -> mov.getId().equals(movie.getId()));
 
         } else {
             movieData = new ArrayList<>();
         }
-        if (moviePresent == null) {
+        if (!isPresent) {
             movieData.add(movie);
             writeJsonData(movieData);
         }
         return movie;
     }
 
+    @Override
+    public List<Movie> getAllMovies() {
+        List<Movie> movieData = readJsonData();
+        return movieData;
+    }
+
 
     @Override
-    public Movie getMovieDetails(String movieId) {
+    public Movie getMovie(String movieId) {
         List<Movie> movieData = readJsonData();
         return movieData.parallelStream().filter(mov -> mov.getId().equals(movieId)).findAny().orElse(null);
     }
