fe69d3d657c62102412d1a0d4f1d73a5e20e66a9
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2022-12-27 20:10:13 +0100

    Add files via upload

--- src/main/java/com/exadel/movie_new/controller/MovieController.java
+++ src/main/java/com/exadel/movie_new/controller/MovieController.java
@@ -1,8 +1,5 @@
 package com.exadel.movie_new.controller;
 
-import com.exadel.movie_new.exception.FilesException;
-import com.exadel.movie_new.exception.GeneralException;
-import com.exadel.movie_new.exception.IdNotValidException;
 import com.exadel.movie_new.exception.MovieNotFoundException;
 import com.exadel.movie_new.model.Movie;
 import com.exadel.movie_new.service.MovieService;
@@ -12,92 +9,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.apache.commons.lang3.StringUtils.*;
-
-
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
 
-
     public MovieController(MovieService movieService) {
-        this.movieService =   movieService;
+        this.movieService = movieService;
     }
-
 
     @PostMapping
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
-        if (!isNumeric(movie.getId())) {
-            throw new IdNotValidException("could not add a movie without valid id");
-        }
         return new ResponseEntity<>(movieService.addMovie(movie), HttpStatus.CREATED);
     }
 
     @GetMapping("/{movieId}")
     public ResponseEntity<Movie> getMovie(@PathVariable String movieId) {
-
-        if (!isNumeric(movieId)) {
-            throw new IdNotValidException("you have to provide a valid Id");
-        }
-        if (movieService.getMovie(movieId) == null) {
-            throw new MovieNotFoundException("requested movie not found");
-        }
-
-        try {
-            return new ResponseEntity<>(movieService.getMovie(movieId), HttpStatus.OK);
-        } catch (Exception e) {
-            throw new GeneralException("something went wrong" + e.getMessage());
-        }
+        return new ResponseEntity<>(movieService.getMovie(movieId), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
-
-
-        if (movieService.getAllMovies().isEmpty()) {
-            throw new MovieNotFoundException("no movies found");
-        }
-
-        try {
-            return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
-        } catch (Exception e) {
-            throw new GeneralException("something went wrong" + e.getMessage());
-        }
+        return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
     }
 
-    @PutMapping("/{movieId}")
+    @PutMapping
     public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie) {
-        if (!isNumeric(movie.getId())) {
-            throw new IdNotValidException("you have to provide a valid Id");
-        }
-        if (movieService.getMovie(movie.getId()) == null) {
-            throw new MovieNotFoundException("requested movie not found");
-        }
-
-        try {
-            return new ResponseEntity<Movie>(movieService.updateMovie(movie), HttpStatus.CREATED);
-        } catch (Exception e) {
-            throw new GeneralException("something went wrong" + e.getMessage());
-        }
+        return new ResponseEntity<>(movieService.updateMovie(movie), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{movieId}")
     public ResponseEntity<Void> deleteMovie(@PathVariable String movieId) {
-
-        if (!isNumeric(movieId)) {
-            throw new IdNotValidException("you have to provide a valid Id");
-        }
-        if (movieService.getMovie(movieId) == null) {
-            throw new MovieNotFoundException("the movie you are trying to delete does not exists");
-        }
-
-        try {
-            movieService.deleteMovie(movieId);
-            return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
-        } catch (Exception e) {
-            throw new GeneralException("something went wrong" + e.getMessage());
-        }
+        movieService.deleteMovie(movieId);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/exadel/movie_new/service/MovieServiceImpl.java
+++ src/main/java/com/exadel/movie_new/service/MovieServiceImpl.java
@@ -1,10 +1,12 @@
 package com.exadel.movie_new.service;
 
 import com.exadel.movie_new.exception.FilesException;
+import com.exadel.movie_new.exception.MovieNotFoundException;
 import com.exadel.movie_new.model.Movie;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Service;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,14 +20,11 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie addMovie(Movie movie) {
         List<Movie> movieData = readJsonData();
-
         boolean isPresent = false;
         if (movieData != null && !movieData.isEmpty()) {
             isPresent = movieData.parallelStream().anyMatch(mov -> mov.getId().equals(movie.getId()));
-
         } else {
             movieData = new ArrayList<>();
-
         }
         if (!isPresent) {
             movieData.add(movie);
@@ -37,18 +36,29 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public List<Movie> getAllMovies() {
         List<Movie> movieData = readJsonData();
+        if (movieData.isEmpty()) {
+            throw new MovieNotFoundException("no movies found");
+        }
         return movieData;
     }
 
     @Override
     public Movie getMovie(String movieId) {
         List<Movie> movieData = readJsonData();
-        return movieData.parallelStream().filter(mov -> mov.getId().equals(movieId)).findAny().orElse(null);
+        Movie searchedMovie = movieData.parallelStream().filter(mov -> mov.getId().equals(movieId)).findAny().orElse(null);
+        if (searchedMovie == null) {
+            throw new MovieNotFoundException("requested movie not found");
+        }
+        return searchedMovie;
     }
 
     @Override
     public Movie updateMovie(Movie movie) {
         List<Movie> movieData = readJsonData();
+        Movie searchedMovie = movieData.parallelStream().filter(mov -> mov.getId().equals(movie.getId())).findAny().orElse(null);
+        if (searchedMovie == null) {
+            throw new MovieNotFoundException("cannot update because requested movie is not found");
+        }
         movieData.removeIf(mov -> mov.getId().equals(movie.getId()));
         movieData.add(movie);
         writeJsonData(movieData);
@@ -58,6 +68,10 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public void deleteMovie(String movieId) {
         List<Movie> movieData = readJsonData();
+        Movie deletedMovie = movieData.parallelStream().filter(mov -> mov.getId().equals(movieId)).findAny().orElse(null);
+        if (deletedMovie == null) {
+            throw new MovieNotFoundException("the movie you are trying to delete does not exists");
+        }
         movieData.removeIf(mov -> mov.getId().equals(movieId));
         writeJsonData(movieData);
     }
@@ -77,15 +91,12 @@ public class MovieServiceImpl implements MovieService {
     }
 
     public void writeJsonData(List<Movie> movieData) {
-
         try (FileWriter file = new FileWriter("movies.json")) {
             file.write(new Gson().toJson(movieData));
             file.flush();
         } catch (IOException e) {
-
             e.printStackTrace();
             throw new FilesException("something went wrong during writing file " + e.getMessage());
         }
     }
-
 }
